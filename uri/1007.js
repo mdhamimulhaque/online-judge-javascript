d6579ecdcb53a72e2f0884e6ae27aef77beaed2a
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-07-22 10:27:28 +0600

    1007

---
+++ uri/1007.js
@@ -0,0 +1,8 @@
+var A = parseInt(readline());
+var B = parseInt(readline());
+var C = parseInt(readline());
+var D = parseInt(readline());
+
+var DIFERENCA = (A * B - C * D);
+
+print("DIFERENCA" + " = " + DIFERENCA)
